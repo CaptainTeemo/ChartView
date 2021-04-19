38a90f749e685a80e8c5411a4e4aa90d909963b9
CaptainTeemo <Chriswoody05@gmail.com>
2021-04-19 11:28:51 +0800

    Improved LineView

--- Sources/SwiftUICharts/LineChart/LineView.swift
+++ Sources/SwiftUICharts/LineChart/LineView.swift
@@ -11,19 +11,22 @@ import SwiftUI
 public struct LineView: View {
     @ObservedObject var data: ChartData
     public var title: String?
-    public var legend: String?
     public var style: ChartStyle
     public var darkModeStyle: ChartStyle
     public var valueSpecifier: String
     public var legendSpecifier: String
     
     @Environment(\.colorScheme) var colorScheme: ColorScheme
-    @State private var showLegend = false
-    @State private var dragLocation:CGPoint = .zero
-    @State private var indicatorLocation:CGPoint = .zero
+    @State private var legendText: String = ""
+    @State private var dragLocation: CGPoint = .zero
+    @State private var indicatorLocation: CGPoint = .zero
     @State private var closestPoint: CGPoint = .zero
-    @State private var opacity:Double = 0
-    @State private var currentDataNumber: Double = 0
+    @State private var opacity: Double = 0
+    @State private var currentDataNumber: Double = 0 {
+        didSet {
+            self.legendText = String(format: "%.2f", self.currentDataNumber)
+        }
+    }
     @State private var hideHorizontalLines: Bool = false
     
     public init(data: [Double],
@@ -35,7 +38,6 @@ public struct LineView: View {
         
         self.data = ChartData(points: data)
         self.title = title
-        self.legend = legend
         self.style = style
         self.valueSpecifier = valueSpecifier!
         self.legendSpecifier = legendSpecifier!
@@ -43,80 +45,66 @@ public struct LineView: View {
     }
     
     public var body: some View {
-        GeometryReader{ geometry in
-            VStack(alignment: .leading, spacing: 8) {
-                Group{
-                    if (self.title != nil){
-                        Text(self.title!)
-                            .font(.title)
-                            .bold().foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
-                    }
-                    if (self.legend != nil){
-                        Text(self.legend!)
-                            .font(.callout)
-                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor : self.style.legendTextColor)
-                    }
-                }.offset(x: 0, y: 20)
-                ZStack{
-                    GeometryReader{ reader in
-                        Rectangle()
-                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.backgroundColor : self.style.backgroundColor)
-                        if(self.showLegend){
-                            Legend(data: self.data,
-                                   frame: .constant(reader.frame(in: .local)), hideHorizontalLines: self.$hideHorizontalLines, specifier: legendSpecifier)
-                                .transition(.opacity)
-                                .animation(Animation.easeOut(duration: 1).delay(1))
-                        }
-                        Line(data: self.data,
-                             frame: .constant(CGRect(x: 0, y: 0, width: reader.frame(in: .local).width - 30, height: reader.frame(in: .local).height + 25)),
-                             touchLocation: self.$indicatorLocation,
-                             showIndicator: self.$hideHorizontalLines,
-                             minDataValue: .constant(nil),
-                             maxDataValue: .constant(nil),
-                             showBackground: false,
-                             gradient: self.style.gradientColor
-                        )
-                        .offset(x: 30, y: 0)
-                        .onAppear(){
-                            self.showLegend = true
-                        }
-                        .onDisappear(){
-                            self.showLegend = false
-                        }
-                    }
-                    .frame(width: geometry.frame(in: .local).size.width, height: 240)
-                    .offset(x: 0, y: 40 )
-                    MagnifierRect(currentNumber: self.$currentDataNumber, valueSpecifier: self.valueSpecifier)
-                        .opacity(self.opacity)
-                        .offset(x: self.dragLocation.x - geometry.frame(in: .local).size.width/2, y: 36)
+        VStack(alignment: .leading, spacing: 8) {
+            Group{
+                if (self.title != nil){
+                    Text(self.title!)
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
                 }
-                .frame(width: geometry.frame(in: .local).size.width, height: 240)
-                .gesture(DragGesture()
-                .onChanged({ value in
-                    self.dragLocation = value.location
-                    self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
-                    self.opacity = 1
-                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
-                    self.hideHorizontalLines = true
-                })
-                    .onEnded({ value in
-                        self.opacity = 0
-                        self.hideHorizontalLines = false
-                    })
+                Text(self.legendText)
+                    .font(.callout)
+                    .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor : self.style.legendTextColor)
+            }
+            GeometryReader { reader in
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.backgroundColor : self.style.backgroundColor)
+                    Legend(data: self.data,
+                           frame: .constant(reader.frame(in: .local)), hideHorizontalLines: self.$hideHorizontalLines, specifier: legendSpecifier)
+                        .transition(.opacity)
+                        .animation(Animation.easeOut(duration: 1).delay(1))
+                    Line(data: self.data,
+                         frame: .constant(reader.frame(in: .local)),
+                         touchLocation: self.$indicatorLocation,
+                         showIndicator: self.$hideHorizontalLines,
+                         minDataValue: .constant(nil),
+                         maxDataValue: .constant(nil),
+                         showBackground: false,
+                         gradient: self.style.gradientColor
+                    )
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            self.dragLocation = value.location
+                            self.indicatorLocation = CGPoint(x: max(value.location.x, 0), y: 32)
+                            self.closestPoint = getClosestDataPoint(toPoint: value.location, width: reader.frame(in: .local).width, height: reader.frame(in: .local).height)
+                            self.opacity = 1
+                            self.hideHorizontalLines = true
+                        })
+                        .onEnded({ value in
+                            self.opacity = 0
+                            self.hideHorizontalLines = false
+                            self.legendText = ""
+                        })
                 )
             }
         }
     }
     
-    func getClosestDataPoint(toPoint: CGPoint, width:CGFloat, height: CGFloat) -> CGPoint {
+    func getClosestDataPoint(toPoint: CGPoint, width: CGFloat, height: CGFloat) -> CGPoint {
         let points = self.data.onlyPoints()
-        let stepWidth: CGFloat = width / CGFloat(points.count-1)
-        let stepHeight: CGFloat = height / CGFloat(points.max()! + points.min()!)
+        let stepWidth = width / CGFloat(points.count - 1)
+        let stepHeight = height / CGFloat(points.max()! + points.min()!)
         
-        let index:Int = Int(floor((toPoint.x-15)/stepWidth))
-        if (index >= 0 && index < points.count){
+        let step = toPoint.x / stepWidth
+        let tail = step - floor(step)
+        let index = Int(tail >= 0.5 ? ceil(step) : floor(step))
+        if (index >= 0 && index < points.count) {
             self.currentDataNumber = points[index]
-            return CGPoint(x: CGFloat(index)*stepWidth, y: CGFloat(points[index])*stepHeight)
+            return CGPoint(x: CGFloat(index) * stepWidth, y: CGFloat(points[index]) * stepHeight)
         }
         return .zero
     }
@@ -126,10 +114,10 @@ struct LineView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             LineView(data: [8,23,54,32,12,37,7,23,43], title: "Full chart", style: Styles.lineChartStyleOne)
+                .preferredColorScheme(.dark)
             
             LineView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Full chart", style: Styles.lineChartStyleOne)
-            
+                .preferredColorScheme(.dark)
         }
     }
 }
-
